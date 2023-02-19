71b0de901742f1039faf898b54fcf58388d0af2a
Eric Pratama <ericpratama50@gmail.com>
2023-02-19 17:29:34 +0700

    404 Not Found

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inconsolata: "'Inconsolata', monospace",
+        spaceMono: "'Space Mono', monospace",
+        montserrat: "'Montserrat', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
